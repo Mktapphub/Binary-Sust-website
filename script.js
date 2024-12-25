a188011e95040c25a1c073077e24eb09c6892daa
Minhajul Khan <128965778+Mktapphub@users.noreply.github.com>
2024-12-25 20:25:09 +0600

    via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sidebar Toggle
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
